03e36f950e48ef642fb4aa789824303c6045f116
danielefongo <danielefongo@gmail.com>
2024-06-18 21:49:43 +0200

    feat(tests.rename): verify renaming across files

--- lua/tests/recode-rust/rename_spec.lua
+++ lua/tests/recode-rust/rename_spec.lua
@@ -110,4 +110,46 @@ pub fn my_function(x: X) {
       helpers.buf_read(main)
     )
   end)
+
+  it("across files", function()
+    local main = helpers.register_buffer(
+      buffers,
+      "lua/tests/example/src/code.rs",
+      [[
+pub fn my_function(param1: i32, param2: i32) {
+  let param2 = param2 + 1;
+}]]
+    )
+
+    local user = helpers.register_buffer(
+      buffers,
+      "lua/tests/example/src/user.rs",
+      [[
+use crate::code;
+pub fn main() {
+  let _ = code::my_function(1, 2);
+}]]
+    )
+
+    local actions = helpers.with_lsp(RustRename.apply, main, Range.new(0, 7, 0, 7), { name = "renamed" })
+
+    helpers.apply_actions(buffers, actions)
+
+    assert.are.same(
+      [[
+pub fn renamed(param1: i32, param2: i32) {
+  let param2 = param2 + 1;
+}]],
+      helpers.buf_read(main)
+    )
+
+    assert.are.same(
+      [[
+use crate::code;
+pub fn main() {
+  let _ = code::renamed(1, 2);
+}]],
+      helpers.buf_read(user)
+    )
+  end)
 end)
